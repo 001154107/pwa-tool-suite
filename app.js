0dd73299db086cae9f2876d51d709290c9590c09
001154107 <tgrbox@gmail.com>
2025-11-19 22:28:19 +1030

    Include Darkmode and improve visuals

--- app.js
+++ app.js
@@ -10,6 +10,9 @@ if ('serviceWorker' in navigator) {
 
 const toolList = document.getElementById('tool-list');
 
+
+
+
 // Fetch tools from the generated JSON file
 fetch('tools.json')
     .then(response => response.json())
@@ -24,3 +27,41 @@ fetch('tools.json')
         });
     })
     .catch(err => console.error('Error loading tools:', err));
+
+// Dark Mode Toggle Logic
+const themeToggleBtn = document.getElementById('theme-toggle');
+const darkIcon = document.getElementById('theme-toggle-dark-icon');
+const lightIcon = document.getElementById('theme-toggle-light-icon');
+
+// Change the icons inside the button based on previous settings
+if (localStorage.getItem('theme') === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+    lightIcon.classList.remove('hidden');
+} else {
+    darkIcon.classList.remove('hidden');
+}
+
+themeToggleBtn.addEventListener('click', function() {
+    // toggle icons inside button
+    darkIcon.classList.toggle('hidden');
+    lightIcon.classList.toggle('hidden');
+
+    // if set via local storage previously
+    if (localStorage.getItem('theme')) {
+        if (localStorage.getItem('theme') === 'light') {
+            document.documentElement.classList.add('dark');
+            localStorage.setItem('theme', 'dark');
+        } else {
+            document.documentElement.classList.remove('dark');
+            localStorage.setItem('theme', 'light');
+        }
+    } else {
+        // if NOT set via local storage previously
+        if (document.documentElement.classList.contains('dark')) {
+            document.documentElement.classList.remove('dark');
+            localStorage.setItem('theme', 'light');
+        } else {
+            document.documentElement.classList.add('dark');
+            localStorage.setItem('theme', 'dark');
+        }
+    }
+});
